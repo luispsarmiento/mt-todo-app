225e2063d10b59a1cdfe1466f3fac1314db45842
Luis Puc <j.louis1493@gmail.com>
2025-02-06 04:12:33 +0000

    Added: retry logic for token refresh and login requests to handle server errors

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { HttpResponse } from '../models/http.model';
-import { catchError, Observable, tap } from 'rxjs';
+import { catchError, concatMap, Observable, retry, tap, throwError, timer } from 'rxjs';
 import { TokenService } from './token.service';
 import { checkToken } from '../interceptors/token.interceptor';
 import { Router } from '@angular/router';
@@ -26,17 +26,39 @@ export class AuthService extends HttpService {
 
   refreshToken(key: string): Observable<any> {
     return this.httpClient.post<HttpResponse<{_id: string}>>(`${environment.baseUrl}${this.endpoint}/refresh-token`, {refreshToken: key}, {context: checkToken()})
-                          .pipe(tap((response: any) => {
-                            this.tokenService.save(response.access_token);
-                            this.tokenService.saveRefreshToken(response.refresh_token);
+                          .pipe(
+                            retry({
+                              count: 3,
+                              delay: (error, count) => {
+                                if (error.status === 500 && count < 3){
+                                  return timer(700);
+                                }
+                                return throwError(error);
+                              },
+                              resetOnSuccess: true
+                            }),
+                            tap((response: any) => {
+                              this.tokenService.save(response.access_token);
+                              this.tokenService.saveRefreshToken(response.refresh_token);
                           }), catchError(this.handleError));
   }
 
   login(email: string, password: string): Observable<any> {
     return this.httpClient.post<HttpResponse<{_id: string}>>(`${environment.baseUrl}${this.endpoint}`, {email: email, password: password}, {context: checkToken()})
-                          .pipe(tap((response: any) => {
-                            this.tokenService.save(response.accessToken);
-                            this.tokenService.saveRefreshToken(response.refreshToken);
+                          .pipe(
+                            retry({
+                              count: 3,
+                              delay: (error, count) => {
+                                if (error.status === 500 && count < 3){
+                                  return timer(700);
+                                }
+                                return throwError(error);
+                              },
+                              resetOnSuccess: true
+                            }),
+                            tap((response: any) => {
+                              this.tokenService.save(response.accessToken);
+                              this.tokenService.saveRefreshToken(response.refreshToken);
                           }), catchError(this.handleError));
   }
 
